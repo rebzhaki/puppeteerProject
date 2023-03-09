93c86a2630da7c4741536fe5add05d6f57c0512a
rebzhaki <rebzhaki@gmail.com>
2023-03-09 16:53:36 +0300

    loop through all pages

--- booking.js
+++ booking.js
@@ -29,23 +29,45 @@ import fs from "fs";
   await page.setJavaScriptEnabled(true);
 
   await page.screenshot({ path: "results1.png", fullPage: true });
-  const docs = await page.$$eval(
-    ".a826ba81c4.fe821aea6c.fa2f36ad22.afd256fc79.d08f526e0d.ed11e24d01.ef9845d4b3.da89aeb942",
-    (elements) =>
-      elements.map((e) => ({
-        title: e.querySelector(".fcab3ed991.a23c043802").innerText,
-        distancefrombeach: e.querySelector(".a196e30dac").innerText,
-        description: e.querySelector(".d8eab2cf7f").innerText,
-      }))
-  );
 
-  // console.log("docs", docs);
-  fs.writeFile("newbookings.json", JSON.stringify(docs), (err) => {
+  // Get the total number of pages
+  const numPages = await page.evaluate(() => {
+    const pages = document.querySelectorAll("div.eef2c3ca89 > ol > li");
+    return pages.length;
+  });
+
+  console.log(`Scraping ${numPages} pages...`);
+
+  const data = [];
+
+  for (let i = 1; i <= numPages; i++) {
+    let docs = await page.$$eval(
+      ".a826ba81c4.fe821aea6c.fa2f36ad22.afd256fc79.d08f526e0d.ed11e24d01.ef9845d4b3.da89aeb942",
+      (elements) =>
+        elements.map((e) => ({
+          // ratings: e.querySelectorAll(".e4755bbd60", (element) =>
+          //   element.getAttribute("aria-label")
+          // ),
+
+          title: e.querySelector(".fcab3ed991.a23c043802").innerText,
+          distanceFromCenter: e.querySelector(
+            "div.a1fbd102d9 > span > span > span"
+          ).innerText,
+          distancefrombeach: e.querySelector(".a196e30dac").innerText,
+          description: e.querySelector(".d8eab2cf7f").innerText,
+          reviews: e.querySelector(".d8eab2cf7f:nth-child(2)").innerText,
+        }))
+    );
+    data.push(docs);
+    await page.click("div.eef2c3ca89 > ol > li");
+  }
+
+  fs.writeFile("bookings.json", JSON.stringify(data), (err) => {
     if (err) throw err;
     console.log("File Saved");
   });
 
-  await page.screenshot({ path: "results.png", fullPage: true });
+  // await page.screenshot({ path: "results.png", fullPage: true });
 
   await browser.close();
 })();
